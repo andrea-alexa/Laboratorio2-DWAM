508233262f82994c152cb66e184115e16db14863
Andrea Núñez <nm0753032023@unab.edu.sv>
2026-03-27 20:53:43 -0600

    Funcion Agregar y mostrar datos

--- app/src/main/java/com/example/appunabandreanunez/MainActivity.kt
+++ app/src/main/java/com/example/appunabandreanunez/MainActivity.kt
@@ -1,55 +1,81 @@
 package com.example.appunabandreanunez
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ListView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import kotlin.jvm.java
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var Nombre: EditText
-    private lateinit var etp_pass: EditText
-    private lateinit var button: Button
+    lateinit var Nombre: EditText
+    lateinit var Edad: EditText
+    lateinit var Departamento: EditText
+    lateinit var btnAgregar: Button
+    lateinit var Lista: ListView
+    lateinit var tvDetalle: TextView
 
+    val listaUsuarios = ArrayList<Usuario>()
+    val listaNombres = ArrayList<String>()
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }*/
+        }
 
         Nombre = findViewById(R.id.Nombre)
-        etp_pass = findViewById(R.id.Edad)
-    }
+        Edad = findViewById(R.id.Edad)
+        Departamento = findViewById(R.id.Departamento)
+        btnAgregar = findViewById(R.id.btnAgregar)
+        Lista = findViewById(R.id.Lista)
+        tvDetalle = findViewById(R.id.tvDetalle)
 
-    fun login(Vista: View) {
-        var nombre = Nombre.text.toString()
-        var pass = etp_pass.text.toString()
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listaNombres)
+        Lista.adapter = adapter
 
-        if (nombre.length == 0){
-            Toast.makeText(this, "Debes escribir tu nombre", Toast.LENGTH_SHORT).show()
+        btnAgregar.setOnClickListener {
+
+            val nombre = Nombre.text.toString()
+            val edad = Edad.text.toString()
+            val departamento = Departamento.text.toString()
+
+            if (nombre.isEmpty() || edad.isEmpty() || departamento.isEmpty()) {
+                Toast.makeText(this, "Complete todos los campos", Toast.LENGTH_SHORT).show()
+            } else {
+                val usuario = Usuario(nombre, edad, departamento)
+
+                listaUsuarios.add(usuario)
+                listaNombres.add(nombre)
+
+                adapter.notifyDataSetChanged()
+
+                // Limpiar campos
+                Nombre.text.clear()
+                Edad.text.clear()
+                Departamento.text.clear()
+            }
         }
 
-        if (pass.length == 0) {
-            Toast.makeText(this, "Debes escribir tu contraseña", Toast.LENGTH_SHORT).show()
-        }
+        Lista.setOnItemClickListener { _, _, position, _ ->
 
-        if (nombre.length != 0 && pass.length != 0) {
-            Toast.makeText(this, "Bienvenido $nombre", Toast.LENGTH_SHORT).show()
-            val newVentana: Intent = Intent(this, MainActivity2::class.java)
-            startActivity(newVentana)
-            Nombre.setText("")
-            etp_pass.setText("")
-        }
-        else{
-            Toast.makeText(this, "Tu usuario o contraseña son incorrectos", Toast.LENGTH_SHORT).show()
+            val usuarioSeleccionado = listaUsuarios[position]
+
+            tvDetalle.text = "Edad: ${usuarioSeleccionado.edad}\nDepartamento: ${usuarioSeleccionado.departamento}"
         }
     }
 }
---
+++ app/src/main/java/com/example/appunabandreanunez/Usuario.kt
@@ -0,0 +1,7 @@
+package com.example.appunabandreanunez
+
+data class Usuario(
+    val nombre: String,
+    val edad: String,
+    val departamento: String
+)
